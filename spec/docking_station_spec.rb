622a5ec6cde357f892c0ed7e0ff5906a1fd143a8
danroche10 <danielroche10@gmail.com>
2021-07-28 15:37:44 +0100

    rspec one line syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "respond to release bike method" do
-    expect(docking_station).to respond_to(release_bike)
+  before(:each) do
+    @docking_station = DockingStation.new
   end
+  it "respond to release bike method" do
+    expect(@docking_station).to respond_to('release_bike')
+  end
+  # one line version of above
+  subject { @docking_station }
+  it { respond_to('release_bike') }
+
+
+
+
+
+
 
 
 end
