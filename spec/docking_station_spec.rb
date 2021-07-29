614cecb9a6b27c0af13740a5a6ad4b63381545eb
Corinne <corinne.boesch@gmail.com>
2021-07-29 14:59:21 +0100

    submit chapter 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,19 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
    before(:each) do
      @docking_station = DockingStation.new
    end
   
-  # it "respond to release bike method" do
-  #   expect(@docking_station).to respond_to('release_bike')
-  # end
-  # one line version of above
-  
-  # test 1 release bike
-  subject { @docking_station }
-  it { respond_to('release_bike') }
-  # test 2 
-  it "To get a bike and check that it is working?" do 
-    expect(@docking_station).to (Bike.new)
+  it "respond to release bike method" do
+    expect(@docking_station).to respond_to('release_bike')
+  end
+  # subject { @docking_station }
+  # it { respond_to('release_bike') }
+
+  it "To get a bike and check that it is working?" do
+    expect(@docking_station.release_bike.working?).to eq(true)
   end
 
 end
